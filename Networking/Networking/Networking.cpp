217ac94d51367f654d970f7cc3aef7c384b38c5e
MaxBABK <mabauer@activision.com>
2022-08-08 09:58:09 -0700

    video code

--- Networking/Networking/Networking.cpp
+++ Networking/Networking/Networking.cpp
@@ -3,9 +3,96 @@
 
 #include <iostream>
 
-int main()
+#include <enet/enet.h>
+
+ENetAddress address;
+ENetHost* server = nullptr;
+ENetHost* client = nullptr;
+
+bool CreateServer();
+bool CreateClient();
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    if (enet_initialize() != 0)
+    {
+        fprintf(stderr, "An error occurred while initializing ENet.\n");
+        return EXIT_FAILURE;
+    }
+    atexit(enet_deinitialize);
+
+    int userType;
+    std::cout << "1 for server 2 for client" << std::endl;
+    std::cin >> userType;
+
+
+    if (userType == 1)
+    {
+        if (!CreateServer())
+        {
+            fprintf(stderr,
+                "An error occurred while trying to create an ENet server host.\n");
+            exit(EXIT_FAILURE);
+        }
+
+    }
+    else
+    {
+        if (!CreateClient())
+        {
+            fprintf(stderr,
+                "An error occurred while trying to create an ENet client host.\n");
+            exit(EXIT_FAILURE);
+        }
+
+    }
+
+
+
+    
+
+
+
+    if (client != nullptr)
+    {
+        enet_host_destroy(client);
+    }
+    
+
+    if (server != nullptr)
+    {
+        enet_host_destroy(server);
+
+    }
+}
+
+
+bool CreateServer()
+{
+    /* Bind the server to the default localhost.     */
+/* A specific host address can be specified by   */
+/* enet_address_set_host (& address, "x.x.x.x"); */
+    address.host = ENET_HOST_ANY;
+    /* Bind the server to port 1234. */
+    address.port = 1234;
+    server = enet_host_create(&address /* the address to bind the server host to */,
+        32      /* allow up to 32 clients and/or outgoing connections */,
+        2      /* allow up to 2 channels to be used, 0 and 1 */,
+        0      /* assume any amount of incoming bandwidth */,
+        0      /* assume any amount of outgoing bandwidth */);
+
+    return server != nullptr;
+}
+
+bool CreateClient()
+{
+
+    client = enet_host_create(NULL /* create a client host */,
+        1 /* only allow 1 outgoing connection */,
+        2 /* allow up 2 channels to be used, 0 and 1 */,
+        0 /* assume any amount of incoming bandwidth */,
+        0 /* assume any amount of outgoing bandwidth */);
+    return client != nullptr;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
