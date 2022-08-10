9cebc07f3931be68b92c6ae58df1f6f790a6d858
MaxBABK <mabauer@activision.com>
2022-08-10 10:09:55 -0700

    MVP guessing game

--- Networking/Networking/Networking.cpp
+++ Networking/Networking/Networking.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <iostream>
 #include <thread>
+#include <atomic>
+#include <time.h>
 
 using namespace std;
 
@@ -10,12 +12,25 @@ ENetPeer* Peer = nullptr;
 bool IsServer = false;
 thread* PacketThread = nullptr;
 
+bool running;
+atomic<bool> canGuess;
+atomic<int> clientGuess;
+int myClientID;
+
+int serverRandValue;
+int currentClientId = 1;
+
+constexpr int MAX_RAND_VAL = 20;
+
 enum PacketHeaderTypes
 {
     PHT_Invalid = 0,
     PHT_IsDead,
     PHT_Position,
-    PHT_Count
+    PHT_Count,
+    PHT_ServerAssignID,
+    PHT_ClientGuess,
+    PHT_ServerGuessResponse
 };
 
 struct GamePacket
@@ -45,6 +60,41 @@ struct PositionPacket : public GamePacket
     int playerId = 0;
     int x = 0;
     int y = 0;
+};
+
+struct ServerAssignIDPacket : public GamePacket
+{
+    ServerAssignIDPacket()
+    {
+        Type = PHT_ServerAssignID;
+    }
+
+    int playerId = 0;
+};
+
+struct ClientGuessPacket : public GamePacket
+{
+    ClientGuessPacket()
+    {
+        Type = PHT_ClientGuess;
+    }
+
+    int playerId = 0;
+    int guess = -1;
+};
+
+struct ServerGuessResponsePacket : public GamePacket
+{
+    ServerGuessResponsePacket()
+    {
+        Type = PHT_ServerGuessResponse;
+    }
+
+    int playerId = 0;
+    int nextPlayerId = 0;
+    bool correct = false;
+    int guess = -1;
+    bool tooHigh = false;
 };
 
 //can pass in a peer connection if wanting to limit
@@ -84,34 +134,27 @@ bool AttemptConnectToServer()
     return Peer != nullptr;
 }
 
-void HandleReceivePacket(const ENetEvent& event)
+void ListenForUserInput()
 {
-    GamePacket* RecGamePacket = (GamePacket*)(event.packet->data);
-    if (RecGamePacket)
+    while (running)
     {
-        cout << "Received Game Packet " << endl;
-
-        if (RecGamePacket->Type == PHT_IsDead)
+        if (canGuess)
         {
-            cout << "u dead?" << endl;
-            IsDeadPacket* DeadGamePacket = (IsDeadPacket*)(event.packet->data);
-            if (DeadGamePacket)
+            int guess;
+            cin >> guess;
+            clientGuess = guess;
+            if (guess < 0)
             {
-                string response = (DeadGamePacket->IsDead ? "yeah" : "no");
-                cout << response << endl;
+                running = false;
             }
+            canGuess = false;
         }
     }
-    else
-    {
-        cout << "Invalid Packet " << endl;
-    }
+}
 
-    /* Clean up the packet now that we're done using it. */
-    enet_packet_destroy(event.packet);
-    {
-        enet_host_flush(NetHost);
-    }
+void SetServerRandVal()
+{
+    serverRandValue = rand() % MAX_RAND_VAL + 1;
 }
 
 void BroadcastIsDeadPacket()
@@ -132,8 +175,204 @@ void BroadcastIsDeadPacket()
     delete DeadPacket;
 }
 
+void RecieveIDFromServer(ServerAssignIDPacket* &packet)
+{
+    myClientID = packet->playerId;
+    cout << "You are player " << myClientID << endl;
+}
+
+void SendClientGuessPacket()
+{
+    ClientGuessPacket* guessPacket = new ClientGuessPacket();
+    guessPacket->guess = clientGuess;
+    guessPacket->playerId = myClientID;
+    ENetPacket* packet = enet_packet_create(guessPacket,
+        sizeof(ClientGuessPacket),
+        ENET_PACKET_FLAG_RELIABLE);
+
+    /* One could also broadcast the packet by         */
+    enet_host_broadcast(NetHost, 0, packet);
+    //enet_peer_send(event.peer, 0, packet);
+
+    /* One could just use enet_host_service() instead. */
+    //enet_host_service();
+    enet_host_flush(NetHost);
+    delete guessPacket;
+}
+
+void HandleServerGuessResponse(ServerGuessResponsePacket*& packet)
+{
+    if (packet->playerId == -1)
+    {
+        // Initial game
+
+    }
+    else
+    {
+        cout << "Guess from player " << packet->playerId << endl;
+        string wasCorrect = "right!";
+        bool correct = packet->correct;
+        string nextOut = "";
+        if (!correct)
+        {
+            nextOut = "It was ";
+            wasCorrect = "wrong!";
+            string notString = "not ";
+            if (packet->tooHigh)
+            {
+                notString = "";
+            }
+            nextOut += notString + "too high.";
+        }
+        else
+        {
+            nextOut = "A new game has started.";
+        }
+        cout << "Guess: " << packet->guess << " was " << wasCorrect << endl;
+        cout << nextOut << endl;
+
+    }
+    
+    cout << "It is now player " << packet->nextPlayerId << "'s turn!" << endl;
+    if (packet->nextPlayerId == myClientID)
+    {
+        canGuess = true;
+    }
+}
+
+
+void InitializeClientWithID(const ENetEvent& event)
+{
+    ServerAssignIDPacket* idPacket = new ServerAssignIDPacket();
+    idPacket->playerId = currentClientId;
+    ENetPacket* packet = enet_packet_create(idPacket,
+        sizeof(ServerAssignIDPacket),
+        ENET_PACKET_FLAG_RELIABLE);
+
+    /* One could also broadcast the packet by         */
+    //enet_host_broadcast(NetHost, 0, packet);
+    enet_peer_send(event.peer, 0, packet);
+
+    /* One could just use enet_host_service() instead. */
+    //enet_host_service();
+    enet_host_flush(NetHost);
+    delete idPacket;
+}
+
+void BroadcastGuessResponse(int player, bool correct, int guess, bool tooHigh)
+{
+    ServerGuessResponsePacket* respPacket = new ServerGuessResponsePacket();
+    respPacket->playerId = player;
+    int nextPlayer;
+    if (player == -1)
+    {
+        nextPlayer = 1;
+    }
+    else
+    {
+        // should do this with mod but....
+        nextPlayer = player + 1;
+        if (nextPlayer >= currentClientId)
+        {
+            nextPlayer = 1;
+        }
+    }
+    
+    respPacket->nextPlayerId = nextPlayer;
+    respPacket->correct = correct;
+    respPacket->guess = guess;
+    respPacket->tooHigh = tooHigh;
+
+    ENetPacket* packet = enet_packet_create(respPacket,
+        sizeof(ServerGuessResponsePacket),
+        ENET_PACKET_FLAG_RELIABLE);
+
+    /* One could also broadcast the packet by         */
+    enet_host_broadcast(NetHost, 0, packet);
+    //enet_peer_send(event.peer, 0, packet);
+
+    /* One could just use enet_host_service() instead. */
+    //enet_host_service();
+    enet_host_flush(NetHost);
+    delete respPacket;
+}
+
+
+void HandleReceivePacket(const ENetEvent& event)
+{
+    GamePacket* RecGamePacket = (GamePacket*)(event.packet->data);
+    if (RecGamePacket)
+    {
+        cout << "Received Game Packet " << endl;
+
+        if (RecGamePacket->Type == PHT_IsDead)
+        {
+            cout << "u dead?" << endl;
+            IsDeadPacket* DeadGamePacket = (IsDeadPacket*)(event.packet->data);
+            if (DeadGamePacket)
+            {
+                string response = (DeadGamePacket->IsDead ? "yeah" : "no");
+                cout << response << endl;
+            }
+        }
+        if (IsServer)
+        {
+            if (RecGamePacket->Type == PHT_ClientGuess)
+            {
+                cout << "guess recieved" << endl;
+                ClientGuessPacket* guessPacket = (ClientGuessPacket*)(event.packet->data);
+                if (guessPacket)
+                {
+                    bool wasCorrect = guessPacket->guess == serverRandValue;
+                    bool tooHigh = guessPacket->guess > serverRandValue;
+                    if (wasCorrect)
+                    {
+                        SetServerRandVal();
+                    }
+                    BroadcastGuessResponse(guessPacket->playerId, wasCorrect, guessPacket->guess, tooHigh);
+                }
+            }
+        }
+        else
+        {
+            if (RecGamePacket->Type == PHT_ServerAssignID)
+            {
+                ServerAssignIDPacket* idPacket = (ServerAssignIDPacket*)(event.packet->data);
+                if (idPacket)
+                {
+                    RecieveIDFromServer(idPacket);
+                }
+            }
+            else if (RecGamePacket->Type == PHT_ServerGuessResponse)
+            {
+                ServerGuessResponsePacket* respPacket = (ServerGuessResponsePacket*)(event.packet->data);
+                if (respPacket)
+                {
+                    HandleServerGuessResponse(respPacket);
+                }
+            }
+
+        }
+    }
+    else
+    {
+        cout << "Invalid Packet " << endl;
+    }
+
+    /* Clean up the packet now that we're done using it. */
+    enet_packet_destroy(event.packet);
+    {
+        enet_host_flush(NetHost);
+    }
+}
+
 void ServerProcessPackets()
 {
+    /* initialize random seed: */
+    srand(time(NULL));
+
+    SetServerRandVal();
+
     while (1)
     {
         ENetEvent event;
@@ -148,7 +387,13 @@ void ServerProcessPackets()
                     << endl;
                 /* Store any relevant client information here. */
                 event.peer->data = (void*)("Client information");
-                BroadcastIsDeadPacket();
+                InitializeClientWithID(event);
+                currentClientId++;
+                if (currentClientId > 2)
+                {
+                    Sleep(500);
+                    BroadcastGuessResponse(-1, false, -1, false);
+                }
                 break;
             case ENET_EVENT_TYPE_RECEIVE:
                 HandleReceivePacket(event);
@@ -166,7 +411,10 @@ void ServerProcessPackets()
 
 void ClientProcessPackets()
 {
-    while (1)
+    canGuess = false;
+    thread inputThread(ListenForUserInput);
+
+    while (running)
     {
         ENetEvent event;
         /* Wait up to 1000 milliseconds for an event. */
@@ -182,7 +430,15 @@ void ClientProcessPackets()
                 break;
             }
         }
+
+        if (clientGuess > 0)
+        {
+            SendClientGuessPacket();
+            clientGuess = 0;
+        }
     }
+
+    inputThread.join();
 }
 
 int main(int argc, char** argv)
@@ -244,6 +500,7 @@ int main(int argc, char** argv)
         cout << "Invalid Input" << endl;
     }
 
+    running = true;
 
     if (PacketThread)
     {
